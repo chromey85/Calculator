efd0534047f65d1f2b87c3e9fd5f780af6bebe1c
Yusuf Ayyub <yayyub85@gmail.com>
2023-05-11 20:32:19 +0100

    calculator finished apart from grid styling

--- main.js
+++ main.js
@@ -29,11 +29,71 @@ keys.addEventListener("click", (event) => {
       el.dataset.state = "";
     });
     key.dataset.state = "selected";
+
+    calculator.dataset.firstNumber = displayValue;
+    calculator.dataset.operator = key.dataset.key;
   }
 
   if (type == "equal") {
-    //perform a calculation
+    //perform a calculation WITHOUT a function
+
+    // const firstNumber = parseInt(calculator.dataset.firstNumber);
+    // const operator = calculator.dataset.operator;
+    // const secondNumber = parseInt(displayValue);
+
+    // let result = "";
+    // if (operator == "plus") result = firstNumber + secondNumber;
+    // if (operator == "minus") result = firstNumber - secondNumber;
+    // if (operator == "divide") result = firstNumber / secondNumber;
+    // if (operator == "times") result = firstNumber * secondNumber;
+
+    // display.textContent = result;
+
+    //perform a calculation WITH a function
+
+    const firstNumber = calculator.dataset.firstNumber;
+    const operator = calculator.dataset.operator;
+    const secondNumber = displayValue;
+
+    display.textContent = calculate(firstNumber, operator, secondNumber);
+  }
+
+  // Clearing the Calculator
+
+  if (type == "clear") {
+    display.textContent = "0";
+    delete calculator.dataset.firstNumber;
+    delete calculator.dataset.operator;
   }
 
   calculator.dataset.previousKeyType = type;
 });
+
+function calculate(firstNumber, operator, secondNumber) {
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+
+  // Calculate with 'if'
+  let result = "";
+  if (operator == "plus") result = firstNumber + secondNumber;
+  if (operator == "minus") result = firstNumber - secondNumber;
+  if (operator == "divide") result = firstNumber / secondNumber;
+  if (operator == "times") result = firstNumber * secondNumber;
+  return result;
+
+  // switch (operator) {
+  //   case "plus":
+  //     result = firstNumber + secondNumber;
+  //     break;
+  //   case "minus":
+  //     result = firstNumber - secondNumber;
+  //     break;
+  //   case "divide":
+  //     result = firstNumber / secondNumber;
+  //     break;
+  //   case "times":
+  //     result = firstNumber * secondNumber;
+  //     break;
+  // }
+  // return result;
+}
